8182cbdee2f7bb13d00a2b9d7f32edac69ad4e07
Joey Patrum (RIT Student) <jdp1701@g.rit.edu>
2025-02-24 11:44:30 -0500

    code skeleton 2(?)

--- src/server.js
+++ src/server.js
@@ -5,24 +5,24 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    notFound: jsonHandler.notFound,
-}
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  notFound: jsonHandler.notFound,
+};
 
 const onRequest = (request, response) => {
-    const protocol = request.connection.encrypted ? 'https' : 'http';
+  const protocol = request.connection.encrypted ? 'https' : 'http';
 
-    // TODO: Parse body & queries
+  // TODO: Parse body & queries
 
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-    if (urlStruct[parsedUrl.pathname]) {
-        return urlStruct[parsedUrl.pathname](request, response);
-    }
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  if (urlStruct[parsedUrl.pathname]) {
+    return urlStruct[parsedUrl.pathname](request, response);
+  }
 
-    return urlStruct.notFound(request, response);
-}
+  return urlStruct.notFound(request, response);
+};
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
